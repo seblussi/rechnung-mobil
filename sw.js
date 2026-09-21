1a9e637611718a1809fb6e8747c80f39f9d4e364
seblussi <sielfr95@googlemail.com>
2026-09-21 12:43:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // App auf dem Handy nach einem Update "haengen" bleibt) die CACHE_NAME-
 // Versionsnummer erhoehen -- nur so erkennt der Browser ueberhaupt, dass
 // sich der Service Worker geaendert hat und tauscht ihn aus.
-const CACHE_NAME = 'rechnungserfassung-v17';
+const CACHE_NAME = 'rechnungserfassung-v18';
 const NETZWERK_ZUERST = ['./Rechnungserfassung_Mobil.html', './manifest.json', './'];
 const CACHE_ZUERST = ['./icd_katalog.js', './icon-192.png', './icon-512.png', './icon-512-maskable.png', './apple-touch-icon.png'];
 
